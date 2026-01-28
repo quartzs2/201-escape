96f6925fd3b541ae87a4e3dcc60c1d7985c81051
quartzs2 <csh001231@gmail.com>
2026-01-29 02:48:32 +0900

    build: initial commit

---
+++ .github/ISSUE_TEMPLATE/feature_request.md
@@ -0,0 +1,15 @@
+---
+name: default
+about: 기본 템플릿
+title: ""
+labels: ""
+assignees:
+---
+
+## 어떤 작업인가요?
+
+> 진행하려는 작업에 대해 간결하게 설명해주세요.
+
+## 작업 상세 내용
+
+- TODO
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
-import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import perfectionist from "eslint-plugin-perfectionist";
+import { defineConfig, globalIgnores } from "eslint/config";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
@@ -13,6 +15,21 @@ const eslintConfig = defineConfig([
     "build/**",
     "next-env.d.ts",
   ]),
+  perfectionist.configs["recommended-natural"],
+  {
+    rules: {
+      curly: ["error", "all"],
+      "react/function-component-definition": [
+        "error",
+        {
+          namedComponents: "function-declaration",
+          unnamedComponents: "arrow-function",
+        },
+      ],
+      "unused-imports/no-unused-imports": "error",
+    },
+  },
+  eslintConfigPrettier,
 ]);
 
 export default eslintConfig;
