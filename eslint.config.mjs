a13e5a72111652392763a26cbb68a33604f334b5
quartzs2 <csh001231@gmail.com>
2026-02-20 20:56:47 +0900

    chore: storybook 초기 설정(#24)
    - storybook 초기 설정

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,6 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
 import eslintConfigPrettier from "eslint-config-prettier/flat";
@@ -36,6 +39,7 @@ const eslintConfig = defineConfig([
     },
   },
   eslintConfigPrettier,
+  ...storybook.configs["flat/recommended"]
 ]);
 
 export default eslintConfig;
